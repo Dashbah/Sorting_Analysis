90099d6732c931b2a59df2fe8df1990416dc202a
dashbah <dsbakhankova@edu.hse.ru>
2023-02-19 20:47:04 +0300

    operation counter added -> finish

--- main.cpp
+++ main.cpp
@@ -12,15 +12,19 @@ std::vector<int> ALMOST_SORTED;
 std::vector<int> REVERSED;
 
 std::vector<std::string> SORTING_NAMES = {
-        "selectionSort", "bubbleSort", "bubbleSort_Aiverson1",
-        "bubbleSortAiverson1And2", "insertion sort", "binaryInsertionSort", "countingSort",
+        "selectionSort", "bubbleSort", "bubbleSort_Iverson1",
+        "bubbleSort_Iverson1And2", "insertion sort", "binaryInsertionSort", "countingSort",
         "digitalSort", "mergeSort", "quickSort", "heapSort", "shellSort_Tsiur",
         "shellSort_Shell"
 };
+std::vector<std::string> ARRAY_NAMES = {
+        "Randomfrom0to5", "Randomfrom0to4100",
+        "AlmostSorted", "Reversed"
+};
 
 int main() {
-    std::ofstream Sorting_CSV_File("sorting.csv");         //Opening file to print info to
-    Sorting_CSV_File << "Sorting by Dashbah" << std::endl;          //Headings for file
+    std::ofstream Sorting_CSV_File("sorting.csv");
+    Sorting_CSV_File << "Sorting by Dashbah" << std::endl;
 
     FROM_0_TO_5 = VectorGenerator::createRandomFromTo(4100, 0, 6);
     FROM_0_TO_4000 = VectorGenerator::createRandomFromTo(4100, 0, 4001);
@@ -28,12 +32,12 @@ int main() {
     REVERSED = VectorGenerator::createReversed(4100);
 
     // vector of all 13 sorting
-    std::vector<int (*)(std::vector<int>&)> sortings = {
-            Sorting::selectionSort, Sorting::bubbleSort, Sorting::bubbleSort_Aiverson1,
-            Sorting::bubbleSortAiverson1And2, Sorting::insertionSort,
-            Sorting::binaryInsertionSort, Sorting::countingSort, Sorting::digitalSort,
-            Sorting::mergeSort, Sorting::quickSort, Sorting::heapSort, Sorting::shellSort_Tsiur,
-            Sorting::shellSort_Shell
+    std::vector<long long (*)(std::vector<int>&)> sorting = {
+            Sorting::selectionSort, Sorting::bubbleSort, Sorting::bubbleSort_Iverson1,
+            Sorting::bubbleSort_Iverson1And2, Sorting::insertionSort,
+            Sorting::binaryInsertionSort, Sorting::countingSort,
+            Sorting::digitalSort, Sorting::mergeSort, Sorting::quickSort,
+            Sorting::heapSort, Sorting::shellSort_Tsiur, Sorting::shellSort_Shell
     };
     // array of iconic arrays
     std::vector<std::vector<int>> arrays = {
@@ -42,24 +46,35 @@ int main() {
 
     // working vector, in which we will copy elements at each sorting
     std::vector<int> new_vec;
-    for (size_t sorting_type = 0; sorting_type < sortings.size(); ++sorting_type) {
-        Sorting_CSV_File << SORTING_NAMES[sorting_type] << ": \n";
+    for (size_t sorting_type = 0; sorting_type < sorting.size(); ++sorting_type) {
+        std::cout << SORTING_NAMES[sorting_type] << "\n";
+        Sorting_CSV_File << SORTING_NAMES[sorting_type] << "\n";
         for (int array_type = 0; array_type < 4; ++array_type) {
-            Sorting_CSV_File << array_type + 1 << " array type\n";
+            Sorting_CSV_File << ARRAY_NAMES[array_type] << "\n";
             Sorting_CSV_File << "\tSmall data:\n\t";
             for (int i = 50; i <= 300; i += 50) {
                 Sorting_CSV_File << i << ",";
                 new_vec  = std::vector<int>(arrays[array_type].begin(), arrays[array_type].begin() + i);
-                auto time = CountTime::countTime(new_vec, sortings[sorting_type]);
-                Sorting_CSV_File << time.ms << ',' << time.operations << '\n';
+                long long average_ms = 0, average_operations = 0;
+                for (int q = 0; q < 10; ++q) {
+                    auto time = CountTime::countTime(new_vec, sorting[sorting_type]);
+                    average_ms += time.ms / 10;
+                    average_operations += time.operations / 10;
+                }
+                Sorting_CSV_File << average_ms << ',' << average_operations << '\n';
             }
             Sorting_CSV_File << '\n';
             Sorting_CSV_File << "Big data: \n";
             for (int i = 100; i <= 4100; i += 100) {
                 Sorting_CSV_File << i << ',';
                 new_vec  = std::vector<int>(arrays[array_type].begin(), arrays[array_type].begin() + i);
-                auto time = CountTime::countTime(new_vec, sortings[sorting_type]);
-                Sorting_CSV_File << time.ms << ',' << time.operations << '\n';
+                long long average_ms = 0, average_operations = 0;
+                for (int q = 0; q < 10; ++q) {
+                    auto time = CountTime::countTime(new_vec, sorting[sorting_type]);
+                    average_ms += time.ms / 10;
+                    average_operations += time.operations / 10;
+                }
+                Sorting_CSV_File << average_ms << ',' << average_operations << '\n';
             }
         }
     }
